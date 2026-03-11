fcf2785c38681276a80ca53fa803ea76556cdb5b
UHarishinkx <your-email@example.com>
2026-03-11 23:00:46 +0530

    Mult Level Cache

--- App/src/assignments.java
+++ App/src/assignments.java
@@ -1,30 +1,36 @@
 import java.util.*;
 
-class TwoSum {
+class MultiLevelCache {
 
-    static int[] findTwoSum(int[] nums,int target){
+    HashMap<String,String> L1=new HashMap<>();
+    HashMap<String,String> L2=new HashMap<>();
+    HashMap<String,String> DB=new HashMap<>();
 
-        HashMap<Integer,Integer> map=new HashMap<>();
+    String getVideo(String id){
 
-        for(int i=0;i<nums.length;i++){
+        if(L1.containsKey(id))
+            return "L1 HIT";
 
-            int complement=target-nums[i];
-
-            if(map.containsKey(complement))
-                return new int[]{map.get(complement),i};
-
-            map.put(nums[i],i);
+        if(L2.containsKey(id)){
+            L1.put(id,L2.get(id));
+            return "L2 HIT -> promoted to L1";
         }
 
-        return new int[]{-1,-1};
+        if(DB.containsKey(id)){
+            L2.put(id,DB.get(id));
+            return "DB HIT -> added to L2";
+        }
+
+        return "Video not found";
     }
 
     public static void main(String[] args){
 
-        int nums[]={2,7,11,15};
+        MultiLevelCache c=new MultiLevelCache();
 
-        int[] res=findTwoSum(nums,9);
+        c.DB.put("video1","data");
 
-        System.out.println(res[0]+" "+res[1]);
+        System.out.println(c.getVideo("video1"));
+        System.out.println(c.getVideo("video1"));
     }
 }
